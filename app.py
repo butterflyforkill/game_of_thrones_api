c78d5a1724af57551e35d976b340f270415bc553
butterflyforkill <vasse.paul@gmail.com>
2024-11-22 21:47:25 +0100

    Change the filtering feature

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask, request, jsonify, abort
 import json_parcer
 
@@ -12,10 +13,28 @@ def get_characters():
     Fetch all characters (with Pagination)
     
     """
+    name = request.args.get('name')
+    house = request.args.get('house')
+    role = request.args.get('role')
+    age_more_than = request.args.get('age_more_than')
+    age_less_than = request.args.get('age_less_than')
     limit = int(request.args.get('limit', 20))
-    skip = int(request.args.get('skip', 0))
+    skip = int(request.args.get('offset', 0))
 
-    paginated_characters = characters[skip:skip + limit]
+    filtered_characters = [char for char in characters if
+                       (not name or char['name'].lower() == name.lower()) and
+                       (not house or str(char['house']).lower().find(house.lower()) >= 0) and
+                       (not role or char['role'].lower() == role.lower()) and
+                       (not age_more_than or char['age'] >= int(age_more_than)) and
+                       (not age_less_than or char['age'] <= int(age_less_than))]
+
+
+    # If no limit or skip is provided, select 20 random characters from the filtered list
+    if not limit and not skip:
+        paginated_characters = random.sample(filtered_characters, 20)
+    else:
+        # Apply pagination to the filtered list
+        paginated_characters = filtered_characters[skip:skip + limit]
 
     return jsonify(paginated_characters)
 
